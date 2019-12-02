ff16042afd20c67961b20335475698a7820efe86
Jarrycow <kjr000614@163.com>
2019-12-02 12:32:06 +0800

    提交about

--- PersonShow/PersonShow/views.py
+++ PersonShow/PersonShow/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+#from gallery.models import Gallery
 
 def home(request):
+    #gallerys = Gallery.description
+    #return render(request,'home.html',{'gallerys':gallerys})
     return render(request,'home.html')
+
+def about(request):
+    return render(request, 'about.html')
